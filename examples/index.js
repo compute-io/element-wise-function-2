d32a48051f1d2cc44d57804a1174b6a41e0f85d7
kgryte <kgryte@gmail.com>
2015-08-18 21:45:59 -0700

    [UPDATE] add example code. Use dstructs-array.

--- examples/index.js
+++ examples/index.js
@@ -1,2 +1,51 @@
 'use strict';
 
+var matrix = require( 'dstructs-matrix' ),
+	array = require( 'dstructs-array' ),
+	ewise = require( './../lib' );
+
+var adder,
+	out;
+
+function add( x, y ) {
+	return x + y;
+}
+
+// Create an element-wise API:
+adder = ewise( add );
+
+// Add numbers:
+out = adder( 3, 5 );
+console.log( 'Number-Number:' );
+console.log( out );
+console.log( '\n' );
+
+// Add a number and an array:
+out = adder( 3, [1,2,3] );
+console.log( 'Number-Array:' );
+console.log( out );
+console.log( '\n' );
+
+// Add a number and a typed array:
+out = adder( array(3,'int8'), 5 );
+console.log( 'Number-Typed Array:' );
+console.log( out );
+console.log( '\n' );
+
+// Add a number and a matrix:
+out = adder( matrix( [2,2], 'int16' ), 5 );
+console.log( 'Number-Matrix:' );
+console.log( out.toString() );
+console.log( '\n' );
+
+// Add array elements:
+out = adder( [1,2,3], array([4,5,6],'float32') );
+console.log( 'Array-Array:' );
+console.log( out );
+console.log( '\n' );
+
+// Add matrix elements:
+out = adder( matrix([1,2,3,4],[2,2]), matrix([5,6,7,8],[2,2]) );
+console.log( 'Matrix-Matrix:' );
+console.log( out );
+console.log( '\n' );
